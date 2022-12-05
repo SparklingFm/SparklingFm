4cd176d6bc161018f1bf02d8b6116f7a0d011ada
Achufam24 <achuulimagbama@gmail.com>
2022-12-05 21:09:57 +0100

    Folder fixes

---
+++ .nuxt/prerender/index.mjs
@@ -0,0 +1,14 @@
+globalThis._importMeta_={url:import.meta.url,env:process.env};export { l as localFetch } from './chunks/nitro/nitro-prerenderer.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/node-fetch-native/dist/polyfill.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/h3/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/ohmyfetch/dist/node.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/destr/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/radix3/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/unenv/runtime/fetch/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/hookable/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/scule/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/ohash/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/unstorage/dist/index.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/unstorage/dist/drivers/fs.mjs';
+import 'file:///Users/achufam/Documents/SparklingFm/node_modules/ufo/dist/index.mjs';
+//# sourceMappingURL=index.mjs.map
